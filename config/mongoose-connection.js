0c1b328e2a5d1f0a95cb0d6d4924a055155074a4
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-25 12:29:56 +0600

    added mongoose connection file

---
+++ config/mongoose-connection.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/aerapackDB")
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+module.exports = mongoose.connection
