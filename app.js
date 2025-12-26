966086cba78f71fff9f3c6e44572210fbd71560b
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-26 11:02:07 +0600

    Base url api test done with postman

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express()
 
 const db = require("./config/mongoose-connection")
 
+const ownersRouter = require("./routes/ownersRouter")
+const usersRouter = require("./routes/usersRouter")
+const productsRouter = require("./routes/productsRouter")
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
@@ -11,9 +15,9 @@ app.get("/owners", ownersRouter)
 app.get("/users", usersRouter)
 app.get("/products", productsRouter)
 
-// app.get("/", (req, res) => {
-//     console.log("Connected")
-// })
+app.get("/", (req, res) => {
+    res.send("Server is running")
+})
 
 app.listen(3000, () => {
     console.log("Server is running")
